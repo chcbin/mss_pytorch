9a601229c2286f8fc5a43c7d52ca3c53c9e96624
S.I. Mimilakis <mis@il077.idmt.fraunhofer.de>
2018-04-27 12:16:33 +0200

    Added training routine for NMR

---
+++ processes_scripts/main_script_nmr.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+__author__ = 'S.I. Mimilakis'
+__copyright__ = 'MacSeNet'
+
+# imports
+import torch
+import torch.optim as optim
+import numpy as np
+import os
+from tqdm import tqdm
+from helpers import visualize, nnet_helpers
+from torch.autograd import Variable
+from modules import cls_sparse_skip_filt as s_s_net
+from losses import loss_functions
+from torch.optim.lr_scheduler import ReduceLROnPlateau as RedLR
+
+mt_path = 'path/to_stored/inverse_masking_threshold_batches/'
+
+
+def main(training, apply_sparsity):
+    """
+        The main function to train and test.
+    """
+    # Reproducible results
+    np.random.seed(218)
+    torch.manual_seed(218)
+    torch.cuda.manual_seed(218)
+    # Torch model
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    # Analysis
+    wsz = 2049   # Window-size
+    Ns = 4096    # FFT size
+    hop = 384    # Hop size
+    fs = 44100   # Sampling frequency
+
+    # Parameters
+    B = 16                      # Batch-size
+    T = 60                      # Length of the sequence
+    N = 2049                    # Frequency sub-bands to be processed
+    F = 744                     # Frequency sub-bands for encoding
+    L = 10                      # Context parameter (2*L frames will be removed)
+    epochs = 100                # Epochs
+    init_lr = 1e-4              # Initial learning rate
+    mnorm = 0.5	                # L2-based norm clipping
+    lambda_h_j_dec = 1e-5       # Scalar for H_j_dec sparsity term
+    p = 2.                      # Error power
+
+    # Data (MUSDB18)
+    totTrainFiles = 100
+    numFilesPerTr = 4
+
+    print('------------   Building model   ------------')
+    encoder = s_s_net.BiGRUEncoder(B, T, N, F, L)
+    decoder = s_s_net.Decoder(B, T, N, F, L, infr=True)
+    sp_decoder = s_s_net.SparseDecoder(B, T, N, F, L)
+    source_enhancement = s_s_net.SourceEnhancement(B, T, N, F, L)
+
+    encoder.train(mode=True)
+    decoder.train(mode=True)
+    sp_decoder.train(mode=True)
+    source_enhancement.train(mode=True)
+
+    if torch.has_cudnn:
+        print('------------   CUDA Enabled   --------------')
+        encoder.cuda()
+        decoder.cuda()
+        sp_decoder.cuda()
+        source_enhancement.cuda()
+
+    # Defining objectives
+    rec_criterion = loss_functions.p_power_error                 # Reconstruction criterion
+
+    optimizer = optim.Adam(list(encoder.parameters()) +
+                           list(decoder.parameters()) +
+                           list(sp_decoder.parameters()) +
+                           list(source_enhancement.parameters()),
+                           lr=init_lr
+                           )
+
+    scheduler = RedLR(optimizer, 'min', factor=0.5, patience=3, verbose=True)
+    if training:
+        win_viz, winb_viz = visualize.init_visdom()
+        batch_loss = []
+        # Over epochs
+        batch_index = 0
+        for epoch in range(epochs):
+            print('Epoch: ' + str(epoch+1))
+            epoch_loss = []
+            # Over the set of files
+            for index in range(totTrainFiles / numFilesPerTr):
+                # Get Data
+                ms, vs = nnet_helpers.get_data(index + 1, numFilesPerTr, wsz, Ns, hop, T, L, B)
+                # Precomputed inverse masking threshold
+                mt = np.load(os.path.join(mt_path, 'masking_threshold_pt_' + str(index) + '.npy'))
+
+                # Shuffle data
+                shf_indices = np.random.permutation(ms.shape[0])
+                ms = ms[shf_indices]
+                vs = vs[shf_indices]
+                mt = mt[shf_indices]
+
+                # Over batches
+                for batch in tqdm(range(ms.shape[0] / B)):
+                    # Mixture to Singing voice
+                    H_enc = encoder.forward(ms[batch * B: (batch + 1) * B, :, :])
+                    H_j_dec = decoder.forward(H_enc)
+                    vs_hat_b = sp_decoder.forward(H_j_dec, ms[batch * B: (batch + 1) * B, :, :])[0]
+                    vs_hat_b_filt = source_enhancement.forward(vs_hat_b)
+
+                    # Loss
+                    loss = rec_criterion(Variable(
+                        torch.from_numpy(vs[batch * B: (batch + 1) * B, L:-L, :]).cuda()),
+                        vs_hat_b_filt,
+                        p
+                    )
+
+                    loss_mask = rec_criterion(
+                        Variable(torch.from_numpy(vs[batch * B: (batch + 1) * B, L:-L, :]).cuda()),
+                        vs_hat_b,
+                        p
+                        )
+
+                    # Accumulated reconstruction loss
+                    loss = torch.log(((loss + loss_mask) * Variable(torch.from_numpy(
+                        mt[batch * B: (batch + 1) * B, L:-L, :]).cuda())).mean() + 1e-6)
+
+                    # Store loss for display and scheduler
+                    batch_loss += [loss.data[0]]
+                    epoch_loss += [loss.data[0]]
+
+                    # Update graph
+                    win_viz = visualize.viz.line(X=np.arange(batch_index, batch_index + 1),
+                                                 Y=np.reshape(batch_loss[batch_index], (1,)),
+                                                 win=win_viz, update='append')
+
+                    # Sparsity term
+                    if apply_sparsity:
+                        sparsity_penalty = H_j_dec.norm(1.) * lambda_h_j_dec
+
+                        loss += sparsity_penalty
+
+                        # Update graph
+                        winb_viz = visualize.viz.line(X=np.arange(batch_index, batch_index + 1),
+                                                      Y=np.reshape(sparsity_penalty.data[0], (1,)),
+                                                      win=winb_viz, update='append')
+
+                    optimizer.zero_grad()
+
+                    loss.backward()
+
+                    torch.nn.utils.clip_grad_norm(list(encoder.parameters()) +
+                                                  list(decoder.parameters()) +
+                                                  list(sp_decoder.parameters()) +
+                                                  list(source_enhancement.parameters()),
+                                                  max_norm=mnorm, norm_type=2)
+                    optimizer.step()
+
+                    batch_index += 1
+
+            if epoch + 1 >= 10:
+                scheduler.step((torch.from_numpy(np.asarray(np.mean(epoch_loss)))))
+
+            if (epoch + 1) % 50 == 0:
+                print('------------   Saving model   ------------')
+                torch.save(encoder.state_dict(), 'results/torch_sps_encoder_l2_log' + str(epoch + 1) + '.pytorch')
+                torch.save(decoder.state_dict(), 'results/torch_sps_decoder_l2_log' + str(epoch + 1) + '.pytorch')
+                torch.save(sp_decoder.state_dict(), 'results/torch_sps_sp_decoder_l2_log' + str(epoch + 1) + '.pytorch')
+                torch.save(source_enhancement.state_dict(), 'results/torch_sps_se_l2_log' + str(epoch + 1) + '.pytorch')
+                print('------------       Done       ------------')
+    else:
+        print('-------  Loading pre-trained model   -------')
+        print('-------  Loading weights  -------')
+        encoder.load_state_dict(torch.load('results/torch_sps_encoder.pytorch'))
+        decoder.load_state_dict(torch.load('results/torch_sps_decoder.pytorch'))
+        sp_decoder.load_state_dict(torch.load('results/torch_sps_sp_decoder.pytorch'))
+        source_enhancement.load_state_dict(torch.load('results/torch_sps_se.pytorch'))
+        print('-------------      Done        -------------')
+
+    return encoder, decoder, sp_decoder, source_enhancement
+
+
+if __name__ == '__main__':
+    training = True          # Whether to train or test the trained model (requires the optimized parameters)
+    apply_sparsity = True    # Whether to apply a sparse penalty or not
+
+    sfiltnet = main(training, apply_sparsity)
+
+# EOF
